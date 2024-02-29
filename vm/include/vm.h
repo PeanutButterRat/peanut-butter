aeb6e39969b5887169da68a5c71badfb62e12be2
Eric Brown <ebrown5676@gmail.com>
2024-02-29 12:11:34 -0800

    Add VM class and executable
    Created a separate executable for the virtual machine and its supplemental class in its own folder structure separate from the compiler

---
+++ vm/include/vm.h
@@ -0,0 +1,8 @@
+#ifndef CPBPL_VM_H
+#define CPBPL_VM_H
+
+class VM {
+
+};
+
+#endif //CPBPL_VM_H
---
+++ vm/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "../include/vm.h"
+
+int main() {
+    VM test{};
+    std::cout << "Hello, world!" << std::endl;
+}
