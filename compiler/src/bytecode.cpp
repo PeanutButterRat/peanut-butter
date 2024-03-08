a2287ed6ec45fb0bd76f98dc5df9416a67d2b59f
Eric Brown <ebrown5676@gmail.com>
2024-03-08 13:10:42 -0800

    Move bytecode functions from compiler file
    Moved the remaining bytecode functions from compiler.cpp to bytecode.cpp. These were left accidentally from the bytecode refactor

--- compiler/src/bytecode.cpp
+++ compiler/src/bytecode.cpp
@@ -18,3 +18,49 @@ Bytecode::Bytecode(std::vector<byte> bytes, std::vector<Value> constants) {
 Value Bytecode::get_constant(size_t index) {
     return constants[index];
 }
+
+size_t Bytecode::add_constant(const Value& constant) {
+    constants.emplace_back(constant);
+    return constants.size() - 1;
+}
+
+void Bytecode::add(byte byte) {
+    bytes.emplace_back(byte);
+}
+
+void Bytecode::disassemble() const {
+    using std::cout, std::endl;
+
+    for (auto it = bytes.begin(); it != bytes.end(); it++) {
+        auto opcode = *it;
+
+        switch (opcode) {
+            case OP_ADD:
+                cout << "ADD" << endl;
+                break;
+            case OP_SUBTRACT:
+                cout << "SUBTRACT"  << endl;
+                break;
+            case OP_MULTIPLY:
+                cout << "MULTIPLY"  << endl;
+                break;
+            case OP_DIVIDE:
+                cout << "DIVIDE"  << endl;
+                break;
+            case OP_CONSTANT: {
+                size_t index = *(++it);
+                Value constant = constants[index];
+                cout << "CONSTANT [index: " << index << ", value: " << constant << ", type: " << constant.get_type_string() << "]" << endl;
+                break;
+            }
+            case OP_MODULO: {
+                cout << "MOD" << endl;
+                break;
+            }
+            default:
+                cout << "[UNKNOWN OPCODE] (" << (unsigned int) opcode << ")" << endl;
+                break;
+        }
+    }
+}
+
